9ce23b86ff54671271a8e8b3c920d4410e452674
rigunkes <richard.monnik@gmail.com>
2025-07-23 16:30:40 -0700

    quartz

---
+++ .github/quartz/quartz.config.ts
@@ -0,0 +1,91 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+/**
+ * Quartz 4.0 Configuration
+ *
+ * See https://quartz.jzhao.xyz/configuration for more information.
+ */
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "The Netyeti's Journal",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    locale: "en-US",
+    baseUrl: "yeticraft.net",
+    ignorePatterns: ["private", "templates", ".obsidian"],
+    defaultDateType: "created",
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Schibsted Grotesk",
+        body: "Source Sans Pro",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#ECEFF4",
+          lightgray: "#E5E9F0",
+          gray: "#4C566A",
+          darkgray: "#434C5E",
+          dark: "#434C5E",
+          secondary: "#5E81AC",
+          tertiary: "#B48EAD",
+          highlight: "rgba(143, 159, 169, 0.15)",
+        },
+        darkMode: {
+          light: "#2E3440",
+          lightgray: "#3B4252",
+          gray: "#ECEFF4",
+          darkgray: "#D8DEE9",
+          dark: "#ECEFF4",
+          secondary: "#5E81AC",
+          tertiary: "#B48EAD",
+          highlight: "rgba(143, 159, 169, 0.15)",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.NotFoundPage(),
+    ],
+  },
+}
+
+export default config
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+# Journal
+My personal journal in technology ...and other things.
+
+## Recent Project Focuses
+- [[Lab Setup/Cloud Init]] - A *needed* thing for Proxmox VMs
+- [[The Stack]] - Defining the tech stack I use most
+- [[WorkAdventure]] - a cool social communication tool with so much more.
